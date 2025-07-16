0757fcf37572b8d0d3eee80ad4e2a82c5a38445d
Pritam Maity <pritammaity071@gmail.com>
2025-07-16 18:46:38 +0530

    Initial commit

--- src/myntra.js
+++ src/myntra.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client"
+
 import Card from "./component/Card";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
@@ -7,12 +8,30 @@ import arr from "./utils/dummy"
 import {greet as goa, meet as roa} from "./utils/dummy"
 
 
+console.log(arr);
+
 function App(){
+
+  let [A, setA] = useState(arr);
+  function sortArray(){
+    A.sort((a,b)=> a.price-b.price);
+    setA([...A]);
+    console.log(arr);
+  }
+
+function priceAbove5000(){
+  const B = arr.filter((value)=>value.price>5000);
+  setA(B);
+
+}
+
   return(<>
     <Header/>
+    <button onClick={sortArray}>Sort By price</button>
+    <button onClick={priceAbove5000}>Price Above 5000</button>
     <div className="middle" style={{display:"flex", gap:"10px" , flexWrap:"wrap"}}>
         {
-          arr.map((value,index)=> <Card key={index} cloth={value.cloth} offer={value.Offer}/>)   
+          A.map((value,index)=> <Card key={index} cloth={value.cloth} offer={value.Offer} price={value.price}/>)   
         }
        
     </div>
